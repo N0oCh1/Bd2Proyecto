4b40853f4081def6c733e5e5230fd22d76d9ffdf
N0oCh1 <110440573+N0oCh1@users.noreply.github.com>
2023-11-21 11:02:31 -0500

    🧪 bloque anonimo para pruebas

---
+++ bloquePrueba.sql
@@ -0,0 +1,11 @@
+-- prueba
+
+BEGIN
+-- cedula, nombre, apellido, correo, telefono, edad
+    add_cliente(4324543, 'nombre1', 'apellido1', 'ejemplo1@ejemplo.com',65474353 ,12);
+    add_cliente(4524543, 'nombre2', 'apellido2', 'ejemplo2@ejemplo.com',65454324 ,22);
+    add_cliente(4624543, 'nombre3', 'apellido3', 'ejemplo3@ejemplo.com',67874332 ,34);
+    add_cliente(4824543, 'nombre4', 'apellido4', 'ejemplo4@ejemplo.com',69876743 ,22);
+    add_cliente(2324543, 'nombre5', 'apellido5', 'ejemplo5@ejemplo.com',61231475 ,23);
+END;
+/
